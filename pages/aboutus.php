9e15622e36f6149ed17d7c4ae056f50ebcdde38a
Shahidul Hasan <er.shahidul@gmail.com>
2014-06-06 15:13:27 +0600

    ok

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -12,7 +12,7 @@ $(document).ready(function(){
 				<div id="bodyR">
 					<div id="animateone" style="font-family : Arial, sans-serif;font-size:12px;margin:0px auto 0px auto;height:273px;width:490px;">
 						<p style="padding:55px 5px 0px 10px">Khaled Group has been doing business in Bangladesh for more than 30 years. Over the years, it has built a reputation of fairness, and honesty in business practice. It stands tall with unmatched ethical standard and yet, leads one of the fastest growing Business Groups in Bangladesh.</p>
-						<p style="padding:0px 5px 0px 10px">With revenue more than 300 Crore taka, and an annual export revenue of over US$10 million, it has built a supply chain that connects all of the sixty four districts in Bangladesh. The Group has visible presence in several industries within Bangladesh and has controlling investment in more than 7 sectors, namely Construction, Engineering, Agricultural, Shipping, Finance, Garments Manufacturing, Power & Energy and Information Technology.</p>
+						<p style="padding:0px 5px 0px 10px">With revenue more than 350 Crore taka, and an annual export revenue of over US$10 million, it has built a supply chain that connects all of the sixty four districts in Bangladesh. The Group has visible presence in several industries within Bangladesh and has controlling investment in more than 7 sectors, namely Construction, Engineering, Agricultural, Shipping, Finance, Garments Manufacturing, Power & Energy and Information Technology.</p>
 					</div>
 					<div id="animate" style="margin-top: -53%; margin-right: 300px;margin-left: 5px;width: 99%;height: 95%;background:#C8C8C8;">
 					</div>
